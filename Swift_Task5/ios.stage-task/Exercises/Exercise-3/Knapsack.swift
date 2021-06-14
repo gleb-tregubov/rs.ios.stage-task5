1b5d559ccc94624db72a3725799a80e19ec0be80
gleb-tregubov <tregubovgleb@gmail.com>
2021-06-14 17:24:05 +0300

    feat: all solutions implemented

--- Swift_Task5/ios.stage-task/Exercises/Exercise-3/Knapsack.swift
+++ Swift_Task5/ios.stage-task/Exercises/Exercise-3/Knapsack.swift
@@ -16,7 +16,36 @@ public final class Knapsack {
         self.foods = foods
     }
     
+    func makeTable(array: [Supply]) -> [[Int]] {
+        var table = Array(repeating: Array(repeating: 0, count: maxWeight + 1), count: array.count + 1)
+        
+        // Пробегаемся по suplies
+        for i in 1...array.count {
+            // Проблегаемся до maxWeight
+            for j in 0...maxWeight {
+                if array[i - 1].weight <= j { // table[i - 1][j] - prev max
+                    table[i][j] = max(table[i - 1][j], array[i - 1].value + table[i - 1][j - array[i - 1].weight])
+//                    table[i][j] = array[i - 1].value
+                } else {
+                    table[i][j] = table[i - 1][j]
+                }
+            }
+        }
+        
+        return table
+        
+    }
+    
     func findMaxKilometres() -> Int {
-        return -1
+        let foodsTable = makeTable(array: self.foods)
+        let drinksTable = makeTable(array: self.drinks)
+        // нужен максимальный дистанц каждый раз в сравнении с предыдущим максимальным дистанцом
+        // берем минимальный value между foods и drinks
+        // сравнивая его с максимальным дистанцом
+        return Array(0...maxWeight).reduce(0) { (res, index) -> Int in
+            return max( res , min( foodsTable[self.foods.count][index] , drinksTable[self.drinks.count][maxWeight - index]))
+        }
+        
+//        return -1
     }
 }
